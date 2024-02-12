311bf54751ac06c4c7d8d29a2ff5f5976bed5013
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-02-12 12:50:16 +0700

    Drawer app bar

--- app/src/main/java/com/tanahku/MainActivity.kt
+++ app/src/main/java/com/tanahku/MainActivity.kt
@@ -2,10 +2,62 @@ package com.tanahku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+import com.tanahku.beranda.BerandaFragment
+import com.tanahku.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener  {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.appBarMain.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(this)
+
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, BerandaFragment())
+                .commit()
+            supportActionBar?.title = getString(R.string.tanah_ku)
+        }
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var fragment: Fragment? = null
+        var title = getString(R.string.app_name)
+        when (item.itemId){
+            R.id.nav_home -> {
+                fragment = BerandaFragment()
+                title = getString(R.string.app_name)
+            }
+        }
+        if(fragment != null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment)
+                .commit()
+        }
+
+        supportActionBar?.title = title
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
